3f55403f4a022e1bf7d766b64169cb400614f097
najlakousi <46430255+najlakousi@users.noreply.github.com>
2019-08-30 00:20:47 +0300

    Add files via upload

---
+++ github (1).py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook for the capstone project.
+# 
+# 
+
+# In[1]:
+
+
+import pandas as dp
+import numpy as np
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
